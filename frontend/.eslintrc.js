9570de8d6b1208a261b9f496e4e5642a668611ce
Chloekkk <skdusdl8804@gmail.com>
2023-04-02 19:25:04 +0900

    💄 style: Update eslint config
    #

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    // Add your own rules here to override ones from the extended configs.
+  },
+};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App()    {
   const [count, setCount] = useState(0)
 
   return (
