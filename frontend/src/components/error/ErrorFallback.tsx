b20009b930027c17b8d9f2e81ad81dad234ecf5e
aLVINlEE9 <fldh3746@gmail.com>
2023-04-06 19:44:07 +0900

    💚 chore: add react-error-boundary
    - Add fallback UI for errors
    - Wrap components in ErrorBoundary
    - Fix conflict

---
+++ frontend/src/components/error/ErrorFallback.tsx
@@ -0,0 +1,16 @@
+interface ErrorFallbackProps {
+  error: Error;
+  resetErrorBoundary: () => void;
+}
+
+const ErrorFallback = ({ error, resetErrorBoundary }: ErrorFallbackProps) => {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: 'red' }}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+};
+
+export default ErrorFallback;
