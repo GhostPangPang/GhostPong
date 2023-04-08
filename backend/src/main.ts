76354b249437a3352708032dcddbfd60e1ec401d
Jisu Kim <hellojs242@gmail.com>
2023-04-08 17:31:50 +0900

    💚  [Chore] Add swagger settings (#37)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppConfigService } from './config/app/configuration.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('GhostPong API')
+    .setDescription('The GhostPong API description')
+    .setVersion('1.0')
+    .addTag('GhostPong')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
   const appConfig: AppConfigService = app.get(AppConfigService);
   await app.listen(appConfig.port);
