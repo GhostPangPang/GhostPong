8a5f62650439b7f83a566a319339013650f6448b
Chloekkk <skdusdl8804@gmail.com>
2023-04-11 23:59:53 +0900

    :green_heart: chore: Update vitest configurations

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
@@ -20,5 +23,13 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
   },
 });
