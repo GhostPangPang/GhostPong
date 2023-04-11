2dfdaf82d5b19d2f945763388651b3d7837c2ec6
Chloekkk <skdusdl8804@gmail.com>
2023-04-12 00:39:29 +0900

    :green_heart: chore: Update test coverage setting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -32,6 +32,9 @@ export default defineConfig({
     setupFiles: './src/test/setup.ts',
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
-    css: true,
+    coverage: {
+      provider: 'c8',
+      include: ['src/**/*.{ts,tsx}'],
+    },
   },
 });
