2f7a2639436d35855899853ddde929f554cad518
Chloekkk <skdusdl8804@gmail.com>
2023-04-13 16:06:19 +0900

    :sparkles: feat: Add GComopnent for common components
    common 컴포넌트에 공통적으로 적용할 GComponent
    정의한 theme 에 따른 공통적인 props 를 제공해줌

--- frontend/src/components/common/GComponent/GComponent.tsx
+++ frontend/src/components/common/GComponent/GComponent.tsx
@@ -1,7 +1,4 @@
 import { forwardRef } from 'react';
-import { GSystemProps, getSystemStyle } from './GSystemStyle';
-import { useTheme } from 'styled-components';
-import { Theme } from '@/assets/styles/types';
 
 type AsProp<T extends React.ElementType> = {
   as?: T;
@@ -25,31 +22,5 @@ export const GComponent: GComponentType = forwardRef(
     const Element = as || 'div';
 
     return <Element ref={ref} {...props} />;
-  },
-);
-
-// SystemStyleProps 처리 + 기존 HTML props 처리 + ref 처리
-// eslint-disable-next-line react/display-name
-export const GStyledComponent: GComponentType = forwardRef(
-  <T extends React.ElementType = 'div'>(
-    { as, ...props }: GComponentProps<T> & GSystemProps,
-    ref: RefProp<T>['ref'],
-  ) => {
-    const Element = as || 'div';
-    const theme = useTheme() as Theme;
-
-    const [sysProps, otherProps] = Object.entries(props).reduce(
-      ([picked, omitted]: [GSystemProps, Record<string, unknown>], [key, value]) => {
-        if (key in theme) {
-          picked[key as keyof GSystemProps] = value;
-        } else {
-          omitted[key] = value;
-        }
-        return [picked, omitted];
-      },
-      [{}, {}],
-    );
-
-    return <Element ref={ref} style={getSystemStyle(theme, sysProps)} {...otherProps} />;
   },
 );
