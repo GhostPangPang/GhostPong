e4abec6c9ea44a1e533052d7f103b80c7849688e
Chloekkk <skdusdl8804@gmail.com>
2023-04-13 16:06:19 +0900

    :sparkles: feat: Add GComopnent for common components
    common 컴포넌트에 공통적으로 적용할 GComponent
    정의한 theme 에 따른 공통적인 props 를 제공해줌

--- frontend/src/components/common/GComponent/GComponent.tsx
+++ frontend/src/components/common/GComponent/GComponent.tsx
@@ -15,25 +15,10 @@ export type GComponentType = <T extends React.ElementType = 'div'>(
 
 // eslint-disable-next-line react/display-name
 export const GComponent: GComponentType = forwardRef(
-  <T extends React.ElementType = 'div'>(
-    { as, ...props }: GComponentProps<T>,
-    ref: React.ComponentPropsWithRef<T>['ref'],
-  ) => {
+  <T extends React.ElementType = 'div'>({ as, ...props }: GComponentProps<T>) => {
     const Element = as || 'div';
     const theme = useTheme() as Theme;
 
-    const [sysProps, otherProps] = Object.entries(props).reduce(
-      ([picked, omitted]: [GSystemProps, Record<string, unknown>], [key, value]) => {
-        if (key in theme) {
-          picked[key as keyof GSystemProps] = value;
-        } else {
-          omitted[key] = value;
-        }
-        return [picked, omitted];
-      },
-      [{}, {}],
-    );
-
-    return <Element ref={ref} style={getSystemStyle(theme, sysProps)} {...otherProps} />;
+    return <Element {...props} style={getSystemStyle(theme, props)} />;
   },
 );
