e7bca628afee7c4ab679b25f5cf642678e0cf7fc
anso33 <hyeon131812@gmail.com>
2023-04-14 23:30:44 +0900

    :sparkles: feature: Set new domain-user

---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,33 @@
+import { Controller } from '@nestjs/common';
+
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto) {
+  //   return this.userService.create(createUserDto);
+  // }
+
+  // @Get()
+  // findAll() {
+  //   return this.userService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.userService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.userService.update(+id, updateUserDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.userService.remove(+id);
+  // }
+}
---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  // create(createUserDto: CreateUserDto) {
+  //   return 'This action adds a new user';
+  // }
+  // findAll() {
+  //   return `This action returns all user`;
+  // }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} user`;
+  // }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
+}
