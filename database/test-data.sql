8ce7bef85b1c530db8f61be4938ba317a7c7d2bf
anso33 <hyeon131812@gmail.com>
2023-04-16 12:38:13 +0900

    :bug: fix: insert sql about fk

--- database/test-data.sql
+++ database/test-data.sql
@@ -1,26 +1,26 @@
 -- user
 INSERT INTO AUTH VALUES(default, 'aaaa@aaaa', NULL, 'UNREGISTERD');
-insert into USERS values(default, 'san', 42, 'asdfasdf');
+insert into USERS values(1, 'san', 42, 'asdfasdf');
 
 --user2
 INSERT INTO AUTH VALUES(default, 'asdf@aaaa', NULL, 'UNREGISTERD');
-insert into USERS values(default, 'jiskim', 11, 'asdf');
+insert into USERS values(2, 'jiskim', 11, 'asdf');
 
 -- user3
 INSERT INTO AUTH VALUES(default, 'qwee@aaaa', NULL, 'UNREGISTERD');
-insert into USERS values(default, 'hannkim', 22, 'qwer');
+insert into USERS values(3, 'hannkim', 22, 'qwer');
 
 -- user4
 INSERT INTO AUTH VALUES(default, 'erty@aaaa', NULL, 'UNREGISTERD');
-insert into USERS values(default, 'nkim', 33, 'erty');
+insert into USERS values(4, 'nkim', 33, 'erty');
 
 -- user5
 INSERT INTO AUTH VALUES(default, 'rtyu@aaaa', NULL, 'UNREGISTERD');
-insert into USERS values(default, 'seungsle', 44, 'rtyu');
+insert into USERS values(5, 'seungsle', 44, 'rtyu');
 
 -- user6
 INSERT INTO AUTH VALUES(default, 'dfgh@aaaa', NULL, 'UNREGISTERD');
-insert into USERS values(default, 'yongjule', 55, 'dfgh');
+insert into USERS values(6, 'yongjule', 55, 'dfgh');
 
 --
 -- blocked_user
