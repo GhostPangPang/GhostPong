0282430072b703d1184337a1f0f5fac83f5525b6
anso33 <hyeon131812@gmail.com>
2023-04-16 11:25:39 +0900

    :bug: fix: Fix insert sql about pk value

--- database/test-data.sql
+++ database/test-data.sql
@@ -1,26 +1,26 @@
--- user1
-INSERT INTO AUTH VALUES(1, 'aaaa@aaaa', NULL, 'UNREGISTERD');
-insert into USERS values(1, 'san', 42, 'asdfasdf');
+-- user
+INSERT INTO AUTH VALUES(default, 'aaaa@aaaa', NULL, 'UNREGISTERD');
+insert into USERS values(default, 'san', 42, 'asdfasdf');
 
 --user2
-INSERT INTO AUTH VALUES(2, 'asdf@aaaa', NULL, 'UNREGISTERD');
-insert into USERS values(2, 'jiskim', 11, 'asdf');
+INSERT INTO AUTH VALUES(default, 'asdf@aaaa', NULL, 'UNREGISTERD');
+insert into USERS values(default, 'jiskim', 11, 'asdf');
 
 -- user3
-INSERT INTO AUTH VALUES(3, 'qwee@aaaa', NULL, 'UNREGISTERD');
-insert into USERS values(3, 'hannkim', 22, 'qwer');
+INSERT INTO AUTH VALUES(default, 'qwee@aaaa', NULL, 'UNREGISTERD');
+insert into USERS values(default, 'hannkim', 22, 'qwer');
 
 -- user4
-INSERT INTO AUTH VALUES(4, 'erty@aaaa', NULL, 'UNREGISTERD');
-insert into USERS values(4, 'nkim', 33, 'erty');
+INSERT INTO AUTH VALUES(default, 'erty@aaaa', NULL, 'UNREGISTERD');
+insert into USERS values(default, 'nkim', 33, 'erty');
 
 -- user5
-INSERT INTO AUTH VALUES(5, 'rtyu@aaaa', NULL, 'UNREGISTERD');
-insert into USERS values(5, 'seungsle', 44, 'rtyu');
+INSERT INTO AUTH VALUES(default, 'rtyu@aaaa', NULL, 'UNREGISTERD');
+insert into USERS values(default, 'seungsle', 44, 'rtyu');
 
 -- user6
-INSERT INTO AUTH VALUES(6, 'dfgh@aaaa', NULL, 'UNREGISTERD');
-insert into USERS values(6, 'yongjule', 55, 'dfgh');
+INSERT INTO AUTH VALUES(default, 'dfgh@aaaa', NULL, 'UNREGISTERD');
+insert into USERS values(default, 'yongjule', 55, 'dfgh');
 
 --
 -- blocked_user
