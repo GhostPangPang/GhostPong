cbd8d967ad3ad8f5025696ef15e36031669bbc54
anso33 <hyeon131812@gmail.com>
2023-04-18 15:13:42 +0900

    :sparkles: feature: Feature api for block user by id

--- backend/src/blocked/blocked.controller.ts
+++ backend/src/blocked/blocked.controller.ts
@@ -1,8 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Headers, Post } from '@nestjs/common';
+import { ApiHeaders, ApiNotFoundResponse, ApiOperation, ApiPreconditionFailedResponse, ApiTags } from '@nestjs/swagger';
+
+import { ErrorResponseDto } from '../common/dto/error-response.dto';
+import { SuccessResponseDto } from '../common/dto/success-response.dto';
 
 import { BlockedService } from './blocked.service';
 
+@ApiTags('blocked')
 @Controller('blocked')
 export class BlockedController {
   constructor(private readonly blockedService: BlockedService) {}
+
+  @ApiOperation({ summary: 'id로 유저 차단하기(토글->마우스 이용)' })
+  @ApiNotFoundResponse({ type: ErrorResponseDto, description: '유저 없음' })
+  @ApiPreconditionFailedResponse({ type: ErrorResponseDto, description: '차단 목록 정원 다참' })
+  @ApiHeaders([{ name: 'x-my-id', description: '내 아이디 (임시값)' }])
+  @Post('blocked/:userId')
+  blockUserById(@Headers('x-my-id') myId: number, userId: number): Promise<SuccessResponseDto> {
+    return this.blockedService.blockUserById(myId, userId);
+  }
 }
--- backend/src/blocked/blocked.service.ts
+++ backend/src/blocked/blocked.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { SuccessResponseDto } from '../common/dto/success-response.dto';
+import { BlockedUser } from '../entity/blocked-user.entity';
+import { User } from '../entity/user.entity';
 
 @Injectable()
-export class BlockedService {}
+export class BlockedService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(BlockedUser)
+    private readonly blockedUserRepository: Repository<BlockedUser>,
+  ) {}
+
+  async blockUserById(myId: number, userId: number): Promise<SuccessResponseDto> {
+    await this.blockedUserRepository.insert({ userId: myId, blockedUserId: userId });
+    return new SuccessResponseDto('유저를 차단하였습니다.');
+  }
+}
