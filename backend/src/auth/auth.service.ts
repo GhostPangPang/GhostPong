e70f6717ab30319bb73a94f583ef5138c791e972
anso33 <hyeon131812@gmail.com>
2023-04-19 14:47:58 +0900

    :hammer: refactor: fix code to validation auth record

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -12,10 +12,11 @@ export class AuthService {
   ) {}
 
   async checkExistAuthId(authId: number): Promise<void> {
-    if ((await this.authRepository.findOneBy({ id: authId })) === null) {
+    const auth = await this.authRepository.findOneBy({ id: authId });
+    if (auth === null) {
       throw new NotFoundException('존재하지 않는 인증 정보입니다.');
     }
-    if ((await this.authRepository.findOneBy({ id: authId, status: AuthStatus.REGISTERD })) !== null) {
+    if (auth.status === AuthStatus.REGISTERD) {
       throw new ConflictException('이미 등록된 유저입니다.');
     }
   }
