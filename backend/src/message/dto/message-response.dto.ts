b4e8290a75664f83e0b76b917d12ec1b5a33d77b
Jisu <hellojs242@gmail.com>
2023-04-21 20:29:02 +0900

    :hammer: refactor: Change type to class in message dto
    - message response dto 를 type -> class 로 변경
    - swagger 를 위한 주석 추가

--- backend/src/message/dto/message-response.dto.ts
+++ backend/src/message/dto/message-response.dto.ts
@@ -1,10 +1,32 @@
-type MessageInfo = {
+class MessageInfo {
+  /**
+   * 메시지 아이디
+   * @example 1
+   */
   id: number;
+
+  /**
+   * 메시지 보낸 사람 아이디
+   * @example 1
+   */
   senderId: number;
+
+  /**
+   * 메세지 내용
+   * @example '안녕하세요'
+   */
   content: string;
+
+  /**
+   * 메세지 보낸 시간
+   * @example '2021-08-01T00:00:00.000Z'
+   */
   createdAt: Date;
-};
+}
 
 export class MessageResponseDto {
+  /**
+   * 메시지 리스트 (32개)
+   */
   messages: MessageInfo[];
 }
