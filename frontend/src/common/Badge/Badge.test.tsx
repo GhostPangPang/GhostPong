1de632f3c7d7fba393e444a76ed87436e4211b44
aLVINlEE9 <fldh3746@gmail.com>
2023-04-22 02:09:31 +0900

    🧪 test: Add test case

--- frontend/src/common/Badge/Badge.test.tsx
+++ frontend/src/common/Badge/Badge.test.tsx
@@ -5,7 +5,7 @@ import { Avatar } from '../Avatar';
 describe('Badge', () => {
   it('should render successfully', () => {
     const { baseElement } = render(
-      <Badge size="sm" status="online">
+      <Badge status="online">
         <Avatar />
       </Badge>,
     );
