4f0552e9e0894e73c057a18c823cc0615b287ad7
Jisu <hellojs242@gmail.com>
2023-04-23 00:56:42 +0900

    :sparkles: feat: Add Upload image API #114
    - image 를 업로드하는 POST /image 를 app.controller 에 추가
    - profile-userid.ext 형식으로 multer 설정
    - 업로드 최대 크기 4MB 설정
    - MIME type png, jpeg, gif 로 restriction

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  FileTypeValidator,
+  Get,
+  HttpStatus,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes, ApiHeaders, ApiOperation } from '@nestjs/swagger';
+import { Response } from 'express';
+import { diskStorage } from 'multer';
 
 import { AppService } from './app.service';
+import { MAX_IMAGE_SIZE } from './common/constant';
 
 @Controller()
 export class AppController {
@@ -9,5 +25,41 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @ApiOperation({ summary: '이미지 업로드' })
+  @ApiConsumes('multipart/form-data')
+  @ApiHeaders([{ name: 'x-my-id', required: true, description: '내 아이디' }])
+  @ApiBody({ schema: { type: 'object', properties: { image: { type: 'string', format: 'binary' } } } })
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: 'public/asset',
+        filename: (req, file, cb) => {
+          // FIXME : auth guard 적용 후 req.user.id로 변경
+          const myId = req.headers['x-my-id'];
+          const extArray = file.mimetype.split('/');
+          cb(null, 'profile-' + myId + '.' + extArray[extArray.length - 1]);
+        },
+      }),
+    }),
+  )
+  @Post('image')
+  uploadImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: MAX_IMAGE_SIZE }),
+          new FileTypeValidator({ fileType: /image\/(png|jpeg|gif)/ }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+    @Res() res: Response,
+  ): void {
+    res.set('Location', file.path.slice(6));
+    res.status(HttpStatus.CREATED).send({
+      message: '이미지 업로드 완료되었습니다.',
+    });
   }
 }
