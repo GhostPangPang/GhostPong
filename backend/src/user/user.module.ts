19e19faea7eaa06ec6a6103d032a05d96c552005
Jisu <hellojs242@gmail.com>
2023-04-24 10:56:08 +0900

    :hammer: refactor: Remove interceptor from provider

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -5,14 +5,13 @@ import { AuthModule } from '../auth/auth.module';
 import { BlockedModule } from '../blocked/blocked.module';
 import { User } from '../entity/user.entity';
 
-import { FileUploadInterceptor } from './interceptor/file-upload.interceptor';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), AuthModule, forwardRef(() => BlockedModule)],
   controllers: [UserController],
-  providers: [UserService, FileUploadInterceptor],
+  providers: [UserService],
   exports: [UserService],
 })
 export class UserModule {}
