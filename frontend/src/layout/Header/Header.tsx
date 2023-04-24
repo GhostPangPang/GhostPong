d8f29a2b1213b990e7a74eeacee166cd42671939
Chloekkk <skdusdl8804@gmail.com>
2023-04-24 18:04:52 +0900

    :hammer: refactor: Refactor HeaderProps

--- frontend/src/layout/Header/Header.tsx
+++ frontend/src/layout/Header/Header.tsx
@@ -1,9 +1,17 @@
 import { ReactComponent as Logo } from '@/svgs/logo-sm.svg';
-import { Grid, GridItemProps } from '../Grid';
+import { Grid } from '../Grid';
 import { Avatar } from '@/common/Avatar';
 import { Dropbox } from '@/common/Dropbox';
+import { Text } from '@/common/Text';
+import { RankProgressBar } from '@/common/RankProgressBar';
 
-export const Header = (props: GridItemProps) => {
+interface HeaderProps {
+  nickname: string;
+  image: string;
+  exp: number;
+}
+
+export const Header = ({ nickname, image, exp }: HeaderProps) => {
   const items = [
     { label: '프로필', onClick: () => console.log('프로필 클릭') },
     { label: '메세지', onClick: () => console.log('메세지 클릭') },
@@ -18,13 +26,16 @@ export const Header = (props: GridItemProps) => {
       justifyContent="space-between"
       alignItems="center"
       size={{ padding: 'header' }}
-      {...props}
+      xs={0}
     >
-      <Logo />
-      <Grid container="flex" justifyContent="end" alignItems="center" gap={1}>
-        nickname
+      <Grid container="flex" justifyContent="start" alignItems="center" xs={2}>
+        <Logo />
+        <RankProgressBar exp={exp} />
+      </Grid>
+      <Grid container="flex" justifyContent="end" alignItems="center" gap={1.5} xs={3}>
+        <Text size="sm">{nickname}</Text>
         <Dropbox items={items} placement="bottomleft">
-          <Avatar size="sm" />
+          <Avatar size="sm" src={image} />
         </Dropbox>
       </Grid>
     </Grid>
