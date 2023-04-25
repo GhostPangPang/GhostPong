4b93aba418e4562afe79e59d410cb49951906b78
Jisu <hellojs242@gmail.com>
2023-04-25 16:03:07 +0900

    :lipstick: style: remove slash at endpoint

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -104,7 +104,7 @@ export class UserController {
 
   @ApiOperation({ summary: '유저 프로필 가져오기' })
   @ApiNotFoundResponse({ type: ErrorResponseDto, description: '존재하지 않는 사용자' })
-  @Get('/:userId/profile')
+  @Get(':userId/profile')
   getUserProfile(@Param('userId') userId: number): Promise<UserProfileResponseDto> {
     return this.userService.getUserProfile(userId);
   }
