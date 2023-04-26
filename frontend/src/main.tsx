d081d8492d0d5a2e85b1d085513ed5486c1eb76b
Chloekkk <skdusdl8804@gmail.com>
2023-04-26 19:02:30 +0900

    :green_heart: chore: Setting react-query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GlobalStyle } from '@/styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import theme from '@/styles/theme';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      suspense: true,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -30,6 +30,9 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    proxy: {
+      '/api': { target: 'http://localhost:5000', changeOrigin: true, rewrite: (path) => path.replace(/^\/api/, '') },
+    },
   },
   test: {
     globals: true,
