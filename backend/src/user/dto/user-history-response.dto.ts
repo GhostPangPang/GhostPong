1606f35097259c4fdc60e67e239b20be31488061
Jisu <hellojs242@gmail.com>
2023-04-26 19:10:46 +0900

    :sparkles: feat: Add UserHistoryResponseDto comments for swagger
    - swagger 에 적용하기 위한 주석 추가

--- backend/src/user/dto/user-history-response.dto.ts
+++ backend/src/user/dto/user-history-response.dto.ts
@@ -1,19 +1,46 @@
 class History {
+  /**
+   * 게임 기록 id
+   * @example 1
+   */
   id: number;
+
+  /**
+   * 게임에서 이긴 유저의 정보
+   */
   winner: {
     id: number;
     nickname: string;
     image: string;
     exp: number;
   };
+
+  /**
+   * 게임에서 진 유저의 정보
+   */
   loser: {
     id: number;
     nickname: string;
     image: string;
     exp: number;
   };
+
+  /**
+   * 게임에서 이긴 유저의 점수
+   * @example 10
+   */
   winnerScore: number;
+
+  /**
+   * 게임에서 진 유저의 점수
+   * @example 5
+   */
   loserScore: number;
+
+  /**
+   * 게임 생성 시간
+   * @example '2021-01-01T00:00:00.000Z'
+   */
   createdAt: Date;
 }
 
