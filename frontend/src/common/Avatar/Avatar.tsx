bef2149d29b034800fa3b70b6ddb3caf72bd69db
aLVINlEE9 <fldh3746@gmail.com>
2023-04-27 17:58:42 +0900

    🔨 refactor: Change boderColor option

--- frontend/src/common/Avatar/Avatar.tsx
+++ frontend/src/common/Avatar/Avatar.tsx
@@ -3,34 +3,37 @@ import profile from '@/svgs/default-profile.svg';
 
 export type AvatarProps = {
   size?: 'sm' | 'md' | 'lg' | 'xl';
-  border?: string;
+  borderColor?: string;
   onClick?: (event: React.MouseEvent<HTMLImageElement>) => void;
 } & React.ComponentPropsWithoutRef<'img'>;
 
 const StyledAvatar = styled.img<AvatarProps>`
   border-radius: 50%;
-  border: ${(props) => props.border || 'none'};
   object-fit: cover;
   cursor: ${(props) => (props.onClick ? 'pointer' : 'default')};
   ${(props) => {
     switch (props.size) {
       case 'sm':
         return `
+        ${props.borderColor ? `border: 0.15rem solid ${props.borderColor};` : ''}
         width: 2.4rem;
         height: 2.4rem;
         `;
       case 'md':
         return `
+        ${props.borderColor ? `border: 0.2rem solid ${props.borderColor};` : ''}
         width: 4rem;
         height: 4rem;
         `;
       case 'lg':
         return `
+        ${props.borderColor ? `border: 0.3rem solid ${props.borderColor};` : ''}
         width: 6.4rem;
         height: 6.4rem;
         `;
       case 'xl':
         return `
+        ${props.borderColor ? `border: 0.5rem solid ${props.borderColor};` : ''}
           width: 16rem;
           height: 16rem;
         `;
