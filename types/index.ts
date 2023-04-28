233fdb9e74300b54b79ee5ba07923654eeca2492
Jisu <hellojs242@gmail.com>
2023-04-28 17:18:51 +0900

    :sparkles: feat: Add index.ts file

---
+++ types/index.ts
@@ -0,0 +1,12 @@
+// export all types from this file
+export * from './blocked/response/blocked-user-response.interface';
+export * from './friend/response/friend-response.interface';
+export * from './friend/response/requested-friend-response.interface';
+export * from './message/response/message-response.interface';
+export * from './user/response/user-info-response.interface';
+export * from './user/response/user-nickname-response.interface';
+export * from './user/response/user-profile-response.interface';
+export * from './user/response/user-history-response.interface';
+export * from './user/request/user-image-request.interface';
+export * from './user/request/user-nickname-request.interface';
+export * from './user/user-info.interface';
