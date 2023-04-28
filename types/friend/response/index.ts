c82ba298cacae6f61aa5887ab9416161b793fe8b
Jisu <hellojs242@gmail.com>
2023-04-28 18:02:47 +0900

    :sparkles: Add index.ts in types request and response directory

---
+++ types/friend/response/index.ts
@@ -0,0 +1,2 @@
+export { FriendResponse } from './friend-response.interface';
+export { RequestedFriendsResponse } from './requested-friend-response.interface';
---
+++ types/user/response/index.ts
@@ -0,0 +1,4 @@
+export { UserHistoryResponse } from './user-history-response.interface';
+export { UserInfoResponse } from './user-info-response.interface';
+export { UserNicknameResponse } from './user-nickname-response.interface';
+export { UserProfileResponse } from './user-profile-response.interface';
