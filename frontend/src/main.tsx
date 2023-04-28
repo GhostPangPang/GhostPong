540a7d2fd384ca32b42d8166d684bf0181283e83
Chloekkk <skdusdl8804@gmail.com>
2023-04-28 12:57:27 +0900

    :green_heart: chore: Setting react-query-devtool

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import App from './App';
 import { GlobalStyle } from '@/styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RecoilRoot } from 'recoil';
 import theme from '@/styles/theme';
 
 const queryClient = new QueryClient({
@@ -18,10 +20,13 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </RecoilRoot>
     </QueryClientProvider>
   </React.StrictMode>,
 );
