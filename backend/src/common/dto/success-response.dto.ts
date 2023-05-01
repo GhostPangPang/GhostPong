4d3dc59bcb0a88cab77ac42802bd4315502585ac
Jisu <hellojs242@gmail.com>
2023-05-01 15:04:37 +0900

    :hammer: refactor: Remove constructor from successResponseDto

--- backend/src/common/dto/success-response.dto.ts
+++ backend/src/common/dto/success-response.dto.ts
@@ -1,9 +1,6 @@
 import { SuccessResponse } from '@/types/common/response';
 
 export class SuccessResponseDto implements SuccessResponse {
-  constructor(message: string) {
-    this.message = message;
-  }
   /**
    * 성공 응답 메세지
    * @example '요청이 처리되었습니다.'
