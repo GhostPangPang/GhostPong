642c9a48cab170eb37708a8342812235ff6ae398
Chloekkk <skdusdl8804@gmail.com>
2023-05-03 19:21:07 +0900

    :sparkles: feat: Add Suspense to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import { MainLayout } from '@/layout/MainLayout';
 import { LobbyPage } from '@/pages/LobbyPage';
 import { PrePage } from './pages/PrePage';
+import { Suspense } from 'react';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/pre" element={<PrePage />} />
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<LobbyPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<h1>Loading</h1>}>
+        <Routes>
+          <Route path="/auth" element={<div>auth</div>} />
+          <Route path="/pre" element={<PrePage />} />
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<LobbyPage />} />
+            {/* <Route path="/profile/:userId" element={<ProfilePage />} /> */}
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
