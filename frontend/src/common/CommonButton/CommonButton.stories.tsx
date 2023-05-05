802e1617918d4357aed2c032a19cfa8fc88fa60d
aLVINlEE9 <fldh3746@gmail.com>
2023-05-05 16:22:23 +0900

    ✨ feature: Add CommonButton

---
+++ frontend/src/common/CommonButton/CommonButton.stories.tsx
@@ -0,0 +1,36 @@
+import type { StoryObj } from '@storybook/react';
+import { CommonButton } from './CommonButton';
+
+const meta = {
+  title: 'Common/CommonButton',
+  component: CommonButton,
+  argTypes: {},
+  decorators: [],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    size: 'md',
+    children: 'Button',
+  },
+};
+
+export const Link: Story = {
+  args: {
+    as: 'a',
+    size: 'md',
+    children: 'Button',
+    href: 'https://naver.com',
+  },
+};
+
+export const Korean: Story = {
+  args: {
+    as: 'button',
+    size: 'md',
+    children: '차단',
+  },
+};
---
+++ frontend/src/common/CommonButton/CommonButton.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import { Color } from '@/types/style';
+import { darken, lighten } from 'polished';
+
+export interface CommonButtonProps {
+  size: 'sm' | 'md' | 'lg';
+  backgroundColor?: Color;
+}
+
+export const CommonButton = styled.button<CommonButtonProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 0.05rem solid;
+  border-radius: 0.375rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  ${(props) => {
+    const bgColor = props.backgroundColor ? props.theme.color[props.backgroundColor] : props.theme.color.gray100;
+    return `
+      background-color: ${bgColor};
+      border-color: ${bgColor};
+      &:hover {
+        background: ${lighten(0.1, bgColor)};
+      }
+      &:active {
+        background: ${darken(0.1, bgColor)};
+      }
+    `;
+  }}
+  ${(props) => {
+    switch (props.size) {
+      case 'sm':
+        return `
+          width: 8rem;
+          height: 3rem;
+          font-size: 1.3rem;
+        `;
+      case 'md':
+        return `
+          width: 12rem;
+          height: 3rem;
+          font-size: 1.3rem;
+        `;
+      case 'lg':
+        return `
+          width: 24rem;
+          height: 3rem;
+          font-size: 1.3rem;
+        `;
+    }
+  }}
+`;
