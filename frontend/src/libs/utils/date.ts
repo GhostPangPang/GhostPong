6244191fe0e34f0ba2760ee87b87c4dec1205ea4
Chloekkk <skdusdl8804@gmail.com>
2023-05-08 14:55:28 +0900

    :sparkles: feat: Update format date lib

---
+++ frontend/src/libs/utils/date.ts
@@ -0,0 +1,20 @@
+export const formatRelativeDate = (date: string | null) => {
+  if (!date) return '';
+  const d = new Date(date);
+  const now = new Date();
+  const diff = now.getTime() - d.getTime();
+  const diffDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+  if (diffDays === 0) {
+    const hour = d.getHours() > 12 ? d.getHours() - 12 : d.getHours();
+    const minute = d.getMinutes() < 10 ? `0${d.getMinutes()}` : d.getMinutes();
+    const ampm = d.getHours() >= 12 ? 'PM' : 'AM';
+    return `${hour}:${minute} ${ampm}`;
+  } else if (diffDays === 1) {
+    return 'Yesterday';
+  } else if (diffDays < 7) {
+    return `${diffDays}days ago`;
+  } else {
+    return `${d.getFullYear()}/${d.getMonth() + 1}/${d.getDate()}`;
+  }
+};
