0c13b7e447d119e532678c6cd07d3827002ba6ad
aLVINlEE9 <fldh3746@gmail.com>
2023-05-08 17:14:53 +0900

    💚 chore: Add args to storybook

--- frontend/src/common/InputBox/InputBox.stories.tsx
+++ frontend/src/common/InputBox/InputBox.stories.tsx
@@ -13,5 +13,5 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: { sizes: 'default' },
+  args: { sizes: 'default', placeholder: 'Default', value: '' },
 };
