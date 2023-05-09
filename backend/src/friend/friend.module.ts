0618789d837cb34a79ec65bcd9ff637f1c533d5c
Jisu <hellojs242@gmail.com>
2023-05-09 14:47:06 +0900

    :sparkles: feat: Add status to get friend API #226

--- backend/src/friend/friend.module.ts
+++ backend/src/friend/friend.module.ts
@@ -5,12 +5,13 @@ import { BlockedUser } from '../entity/blocked-user.entity';
 import { Friendship } from '../entity/friendship.entity';
 import { MessageView } from '../entity/message-view.entity';
 import { User } from '../entity/user.entity';
+import { RepositoryModule } from '../repository/repository.module';
 
 import { FriendController } from './friend.controller';
 import { FriendService } from './friend.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Friendship, BlockedUser, MessageView, User])],
+  imports: [TypeOrmModule.forFeature([Friendship, BlockedUser, MessageView, User]), RepositoryModule],
   controllers: [FriendController],
   providers: [FriendService],
 })
