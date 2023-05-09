adff423298cb5fc363fc9d9d479889e5337e7594
anso33 <hyeon131812@gmail.com>
2023-05-10 06:41:13 +0900

    :sparkles: feat: Feat socket event for connect
    가장 기본적인 소켓 연결과 삭제에 대한 이벤트 구현했습니다.

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,4 +1,5 @@
 import { Module, forwardRef } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppConfigModule } from 'src/config/app/configuration.module';
@@ -8,18 +9,22 @@ import { Achievement } from '../entity/achievement.entity';
 import { GameHistory } from '../entity/game-history.entity';
 import { UserRecord } from '../entity/user-record.entity';
 import { User } from '../entity/user.entity';
+import { RepositoryModule } from '../repository/repository.module';
 
 import { UserController } from './user.controller';
+import { UserGateway } from './user.gateway';
 import { UserService } from './user.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Achievement, UserRecord, GameHistory]),
-    forwardRef(() => AuthModule),
     AppConfigModule,
+    forwardRef(() => AuthModule),
+    RepositoryModule,
+    JwtModule.register({}),
   ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserGateway],
   exports: [UserService],
 })
 export class UserModule {}
