c386912e09f92a3499c7ff3485afa7d80e664b67
aLVINlEE9 <fldh3746@gmail.com>
2023-05-09 14:21:53 +0900

    🐛 fix: Fix edit page

--- frontend/src/pages/EditProfilePage/EditProfilePage.tsx
+++ frontend/src/pages/EditProfilePage/EditProfilePage.tsx
@@ -3,9 +3,9 @@ import { Text } from '@/common/Text';
 import { Box } from '@/common/Box';
 import styled from 'styled-components';
 import { Avatar } from '@/common/Avatar';
-import { backgrounds } from 'polished';
-// import { CommonButton } from '@/common/CommonButton';
-import { darken, lighten } from 'polished';
+import { CommonButton } from '@/common/Button/CommonButton';
+import { InputBox } from '@/common/InputBox';
+import { Toggle } from '@/common/Toggle';
 
 interface EditFormProps {
   desc: string;
@@ -20,65 +20,6 @@ const StyledLine = styled.div`
   background-color: ${(props) => props.theme.color.gray200};
 `;
 
-const ChcekBox = styled.input`
-  width: 1.5rem;
-  height: 1.5rem;
-  border: 0.05rem solid;
-  font-size: 1.5rem;
-  background-color: transparent;
-  border-radius: 0.375rem;
-  border-color: white;
-  color: white;
-`;
-
-const InputBox = styled.input`
-  width: 50%;
-  height: 3rem;
-  border: 0.05rem solid;
-  font-size: 1.5rem;
-  background-color: transparent;
-  border-radius: 0.375rem;
-  border-color: white;
-  color: white;
-`;
-
-const EditButton = styled.button`
-  width: 20%;
-  height: 100%;
-  border: 0.05rem solid;
-  font-size: 1.5rem;
-  background-color: ${(props) => props.theme.color.gray100};
-  border-radius: 0.375rem;
-  border-color: white;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-  &:hover {
-    background: ${(props) => lighten(0.1, props.theme.color.gray100)};
-  }
-  &:active {
-    background: ${(props) => darken(0.1, props.theme.color.gray100)};
-  }
-`;
-
-const EditButton2 = styled.button`
-  width: 20%;
-  height: 100%;
-  border: 0.05rem solid;
-  font-size: 1.5rem;
-  background-color: ${(props) => props.theme.color.gray200};
-  border-radius: 0.375rem;
-  border-color: #ffffff;
-  color: #ffffff;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-  &:hover {
-    background: ${(props) => lighten(0.1, props.theme.color.gray100)};
-  }
-  &:active {
-    background: ${(props) => darken(0.1, props.theme.color.gray100)};
-  }
-`;
-
 const EditForm = ({ desc, label, value, children }: EditFormProps) => {
   return (
     <Grid container="grid" rows={1} columns={3}>
@@ -87,7 +28,7 @@ const EditForm = ({ desc, label, value, children }: EditFormProps) => {
       </Grid>
       <Grid gridColumn="span 2/span 2" size={{ height: '100%', padding: 'md' }}>
         <Grid container="flex" direction="column" justifyContent="center" alignItems="center">
-          <Box backgroundColor="gray300">
+          <Box backgroundColor="gray300" width="64rem">
             <Grid
               container="flex"
               direction="column"
@@ -101,9 +42,9 @@ const EditForm = ({ desc, label, value, children }: EditFormProps) => {
               {children}
             </Grid>
           </Box>
-          <Box backgroundColor="gray200">
+          <Box backgroundColor="gray200" width="64rem">
             <Grid container="flex" justifyContent="end" alignItems="center" size={{ height: '100%', padding: 'sm' }}>
-              <EditButton>{value}</EditButton>
+              <CommonButton size="md">{value}</CommonButton>
             </Grid>
           </Box>
         </Grid>
@@ -116,25 +57,25 @@ export const EditProfilePage = () => {
   return (
     <Grid container="flex" direction="column" justifyContent="center" size={{ maxWidth: '100rem' }}>
       <EditForm desc="Account Information" label="UserName" value="Save">
-        <InputBox type="text" />
-      </EditForm>
-      <StyledLine />
-      <EditForm desc="Email" label="Email" value="Save">
-        <Grid container="flex" justifyContent="space-between" alignItems="center">
-          <InputBox type="text" />
-          <Grid container="flex" justifyContent="end" alignItems="center" gap={2}>
-            <Text size="xs" color="gray100">
-              2FA Setting
-            </Text>
-            <ChcekBox type="checkbox" />
-          </Grid>
-        </Grid>
+        <InputBox sizes="sm" value="ghostking" placeholder="your nickname" />
       </EditForm>
       <StyledLine />
       <EditForm desc="Profile" label="Avatar" value="Upload & Save">
         <Grid container="flex" alignItems="center" gap={3}>
           <Avatar size="lg" />
-          <EditButton2>Upload</EditButton2>
+          <CommonButton size="md">Upload</CommonButton>
+        </Grid>
+      </EditForm>
+      <StyledLine />
+      <EditForm desc="Two-factor authentication" label="Email" value="Verify">
+        <Grid container="flex" justifyContent="space-between" alignItems="center">
+          <InputBox sizes="md" value="ghostking@gmail.com" placeholder="your email" />
+          <Grid container="flex" justifyContent="end" alignItems="center" gap={2}>
+            <Text size="sm" color="gray100" weight="bold">
+              2FA
+            </Text>
+            <Toggle />
+          </Grid>
         </Grid>
       </EditForm>
     </Grid>
