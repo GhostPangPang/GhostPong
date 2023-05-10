05c6a1596e96c19185513df1534680bcc6790d32
aLVINlEE9 <fldh3746@gmail.com>
2023-05-11 01:38:44 +0900

    ✨ feature: Add useFileUpload mutation hook

---
+++ frontend/src/hooks/useFileUpload.tsx
@@ -0,0 +1,63 @@
+import { post, patch } from '@/libs/api';
+import { useMutation } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+import { ChangeEvent, useState } from 'react';
+
+const API = '/user/image';
+
+const postFileUpload = async (formData: FormData): Promise<AxiosResponse> => {
+  return await post<AxiosResponse>(API, formData);
+};
+
+const patchFileUpload = async (location: string): Promise<AxiosResponse> => {
+  return await patch<AxiosResponse>(API, { image: location });
+};
+
+export const useFileUpload = () => {
+  const [selectedFile, setSelectedFile] = useState<File>();
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+  const patchFileUploadMutation = useMutation(
+    (location: string) => {
+      if (!location) return Promise.reject(new Error('No location selected'));
+      return patchFileUpload(location);
+    },
+    {
+      onSuccess: (data) => {
+        alert('프로필 사진이 변경되었습니다.');
+        console.log(data);
+      },
+      onError: (error) => {
+        alert('프로필 사진 변경에 실패하였습니다.');
+        console.log(error);
+        // return Promise.reject(new Error('File Patch Failured'));
+      },
+    },
+  );
+
+  const fileUploadMutation = useMutation(
+    () => {
+      if (!selectedFile) return Promise.reject(new Error('No file selected'));
+
+      const formData = new FormData();
+      formData.append('image', selectedFile);
+      return postFileUpload(formData);
+    },
+    {
+      onSuccess: (data) => {
+        patchFileUploadMutation.mutate(data.headers.location);
+      },
+      onError: (error) => {
+        alert('파일 업로드에 실패하였습니다.');
+        console.log(error);
+        // return Promise.reject(new Error('File Upload Failured'));
+      },
+    },
+  );
+
+  return { handleFileChange, fileUploadMutation };
+};
