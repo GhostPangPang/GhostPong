67339620d78f4126e790de7ee3aa9398ab6b3dc2
Chloekkk <skdusdl8804@gmail.com>
2023-05-10 16:21:01 +0900

    :sparkles: feat: Update auth state type

--- frontend/src/hooks/useAuth.tsx
+++ frontend/src/hooks/useAuth.tsx
@@ -1,25 +1,45 @@
 import { get } from '@/libs/api';
-import { UserInfo, useUserInfo } from '@/stores/userInfo';
+import { useUserInfo } from '@/stores/userInfo';
+import { UserInfoResponse } from '@/dto/user/response';
 import { useQuery } from '@tanstack/react-query';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const API = '/user';
 
 const getUserInfo = async () => {
-  return await get<UserInfo>(API);
+  const data = await get<UserInfoResponse>(API.USER.INFO);
+};
+
+const initialData: UserInfoResponse = {
+  id: -1,
+  nickname: '',
+  image: '',
+  exp: 0,
+  blockedUsers: [],
 };
 
 export const useAuth = () => {
+  const navigate = useNavigate();
   const { userInfo, setUserInfo } = useUserInfo();
-  const { data, isLoading, refetch } = useQuery<UserInfo>({
-    queryKey: [API],
+  const {
+    data = initialData,
+    isLoading,
+    refetch,
+  } = useQuery<UserInfoResponse>({
+    queryKey: [API.USER.INFO],
     queryFn: getUserInfo,
     retryOnMount: true,
     staleTime: Infinity,
+    onError: (error) => {
+      console.log('useAuth error', error);
+    },
   });
 
   useEffect(() => {
+    console.log('useAuth data', data);
     if (data) setUserInfo(data);
+    else navigate('/pre');
   }, [data]);
 
   return { userInfo, isLoading, refetch };
