41ced2f793698d2d39b6dad88800a12d47dde43d
aLVINlEE9 <fldh3746@gmail.com>
2023-05-10 15:41:50 +0900

    💚 chore: Add Route for edit page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { PrePage } from '@/pages/PrePage';
 import { MessagePage } from '@/pages/MessagePage';
 import { ProfilePage } from './pages/ProfilePage';
 import { ErrorBoundary } from 'react-error-boundary';
+import { EditProfilePage } from './pages/EditProfilePage';
 
 function App() {
   return (
@@ -22,7 +23,7 @@ function App() {
               <Route path="/message" element={<MessagePage />} />
             </Route>
             <Route element={<ScollableLayout />}>
-              <Route path="/profile/edit" element={<ProfilePage />} />
+              <Route path="/profile/edit" element={<EditProfilePage />} />
               <Route path="/profile/:userId" element={<ProfilePage />} />
             </Route>
           </Routes>
