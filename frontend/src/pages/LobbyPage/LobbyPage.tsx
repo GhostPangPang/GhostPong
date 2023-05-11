46becdc4ce7e737ddac95d9ab4f7d1c99d721912
Chloekkk <skdusdl8804@gmail.com>
2023-05-11 15:40:25 +0900

    :bug: fix: Fix Ghost Component

--- frontend/src/pages/LobbyPage/LobbyPage.tsx
+++ frontend/src/pages/LobbyPage/LobbyPage.tsx
@@ -1,6 +1,6 @@
+import { Ghost } from '@/common';
 import { GameButton } from '@/common/Button/GameButton';
 import { Grid } from '@/common/Grid';
-import Rive from '@rive-app/react-canvas';
 import { useNavigate } from 'react-router-dom';
 
 export const LobbyPage = () => {
@@ -9,7 +9,7 @@ export const LobbyPage = () => {
   return (
     <Grid container="grid" rows={2} columns={3} rowsSize={[1, 0]} columnsSize={[1, 2, 1]} size={{ height: '100%' }}>
       <Grid gridColumn="2" alignSelf="center" justifySelf="center" size={{ height: '360px', width: '360px' }}>
-        <Rive src="/riv/ghost.riv" />
+        <Ghost />
       </Grid>
       <Grid gridColumn="1" gridRow="2" alignSelf="end">
         <GameButton size="md" onClick={() => navigate('/message')}>
