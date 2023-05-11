6fe791620cad2951f445becc639b74bc1be8dc47
Chloekkk <skdusdl8804@gmail.com>
2023-05-11 15:41:15 +0900

    :sparkles: feat: Add errorInterceptor client.s

--- frontend/src/libs/api/client.ts
+++ frontend/src/libs/api/client.ts
@@ -1,10 +1,10 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import { getAccessToken } from './auth';
 
 const createAxiosInstance = () => {
   const isDev = import.meta.env.MODE === 'development';
   const token = getAccessToken();
-  const headers = isDev ? { 'x-my-id': '1' } : token ? { Authorization: `Bearer ${token}` } : {};
+  const headers = isDev ? { 'x-my-id': '1' } : { Authorization: `Bearer ${token}` };
 
   return axios.create({ baseURL: import.meta.env.VITE_API_URL, headers, timeout: 100000, withCredentials: true });
 };
@@ -13,12 +13,32 @@ const responseInterceptor = (res: AxiosResponse) => {
   if (res.status >= 200 && res.status < 300) {
     return res.data;
   }
-  return Promise.reject(res.data);
+  return Promise.reject(res);
+};
+
+const errorInterceptor = (error: AxiosError<ApiError>) => {
+  if (error.response) {
+    const { status, data } = error.response;
+
+    if (status === 401) {
+      localStorage.removeItem('accessToken');
+      location.replace('/pre');
+    }
+    const message = data?.message || '알 수 없는 에러가 발생했습니다.';
+    return Promise.reject({ statusCode: status, message, error: data?.error });
+  }
+  return Promise.reject({ statusCode: 500, message: '서버와의 연결이 끊어졌습니다.' });
 };
 
 export const client = createAxiosInstance();
-client.interceptors.response.use(responseInterceptor);
+client.interceptors.response.use(responseInterceptor, errorInterceptor);
 // 여기서 auth req interceptor 로 customAuthError 발생시키기 -> ErrorBoundary 에서 해결
+
+export type ApiError = {
+  statusCode: number;
+  message?: string;
+  error?: string;
+};
 
 export const get = <T>(...args: Parameters<typeof client.get>) => {
   return client.get<T, T>(...args);
