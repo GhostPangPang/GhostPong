430590b508c18cc21d5bd0d4092ab15941b04173
anso33 <hyeon131812@gmail.com>
2023-05-13 20:49:50 +0900

    :hammer: refactor: Delete useless event code
    - 더 이상 필요 없어진 socket room 관련 코드 삭제했습니다.

--- backend/src/friend/friend.gateway.ts
+++ backend/src/friend/friend.gateway.ts
@@ -1,6 +1,6 @@
 import { InjectRepository } from '@nestjs/typeorm';
-import { ConnectedSocket, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 import { Repository } from 'typeorm';
 
 import { corsOption } from '../common/option/cors.option';
@@ -17,30 +17,6 @@ export class FriendGateway {
     @InjectRepository(Friendship)
     private readonly friendshipRepository: Repository<Friendship>,
   ) {}
-
-  /**
-   * 친구 창에 들어왔을 때 모든 친구와의 friend room 에 join 한다.
-   *
-   * @param socket 이벤트를 보낸 유저의 소켓
-   */
-  @SubscribeMessage('join-friend-room')
-  async joinFriendRoom(@ConnectedSocket() socket: Socket): Promise<void> {
-    (await this.findFriendsId(socket.data.userId)).map(({ id }) => {
-      socket.join(`friend-${id}`);
-    });
-  }
-
-  /**
-   * 친구 창을 나갔을 때 모든 친구와의 friend room 에서 leave 한다.
-   *
-   * @param socket 이벤트를 보낸 유저의 소켓
-   */
-  @SubscribeMessage('leave-friend-room')
-  async leaveFriendRoom(@ConnectedSocket() socket: Socket): Promise<void> {
-    (await this.findFriendsId(socket.data.userId)).map(({ id }) => {
-      socket.leave(`friend-${id}`);
-    });
-  }
 
   /**
    * 친구 요청이 수락되었을 때 친구 요청을 보낸 유저에게 이벤트를 보낸다.
@@ -63,15 +39,6 @@ export class FriendGateway {
   }
 
   // SECTION: private
-  private findFriendsId(userId: number): Promise<Friendship[]> {
-    return this.friendshipRepository.find({
-      select: ['id'],
-      where: [
-        { senderId: userId, accept: true },
-        { receiverId: userId, accept: true },
-      ],
-    });
-  }
 
   //!SECTION: private
 }
