b92091ad5b8ea56af29e28f8bb65cd01f1335bed
Kimhannah <skqkdldhf98@naver.com>
2023-05-13 23:00:15 +0900

    :bug: fix: Modify nickname response to SuccessResponseDto

--- backend/src/user/dto/response/user-nickname-response.dto.ts
+++
@@ -1,9 +0,0 @@
-import { UserNicknameResponse } from '@/types/user/response';
-
-export class UserNicknameResponseDto implements UserNicknameResponse {
-  /**
-   * nickname
-   * @example 'san1'
-   */
-  nickname: string;
-}
