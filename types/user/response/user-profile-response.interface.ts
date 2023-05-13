7af9b0d92a82f81441a7ae29cf66b3f9146cd99d
anso33 <hyeon131812@gmail.com>
2023-05-13 18:58:02 +0900

    :bug: fix: Fix double quote to single

--- types/user/response/user-profile-response.interface.ts
+++ types/user/response/user-profile-response.interface.ts
@@ -1,4 +1,4 @@
-import { UserInfo } from "../user-info.interface";
+import { UserInfo } from '../user-info.interface';
 
 export interface UserProfileResponse extends UserInfo {
   nickname: string;
