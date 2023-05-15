3695b9aac80ddba90e81cb2c578173649fdf09da
aLVINlEE9 <fldh3746@gmail.com>
2023-05-15 19:50:40 +0900

    🐛 fix: Fix build error

--- frontend/src/common/Toggle/Toggle.stories.tsx
+++ frontend/src/common/Toggle/Toggle.stories.tsx
@@ -13,5 +13,10 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    toggle: true,
+    onChange: () => {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+    },
+  },
 };
