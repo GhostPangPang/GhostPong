6ef4d419a608f45b66aaec39dac57e2773e759a3
Jisu <hellojs242@gmail.com>
2023-05-15 23:34:25 +0900

    :hammer: refactor: Modify users type Array to Map
    - id 서칭을 쉽게 하기 위한 Map 타입	변경

--- backend/src/repository/model/channel.ts
+++ backend/src/repository/model/channel.ts
@@ -1,17 +1,33 @@
 export type ChannelMode = 'private' | 'public' | 'protected';
 export type ChannelRole = 'owner' | 'admin' | 'common';
+export class ChannelUser {
+  constructor(role: ChannelRole, id: number, nickname: string, image?: string) {
+    this.id = id;
+    this.nickname = nickname;
+    this.image = image;
+    this.role = role;
+    this.isPlayer = role === 'owner' ? true : false;
+  }
+
+  id: number;
+  nickname: string;
+  image?: string;
+  role: ChannelRole;
+  isMuted = false;
+  isPlayer: boolean;
+}
 
 export class Channel {
+  constructor(mode: ChannelMode, name: string, password?: string) {
+    this.mode = mode;
+    this.name = name;
+    this.password = password;
+    this.users = new Map<number, ChannelUser>();
+  }
+
   name: string;
-  userList: {
-    id: number;
-    nickname: string;
-    image: string;
-    role: ChannelRole;
-    isMuted: boolean;
-    isPlayer: boolean;
-  }[];
   mode: ChannelMode = 'public';
   password?: string;
+  users: Map<number, ChannelUser>;
   bannedUserIdList?: number[];
 }
