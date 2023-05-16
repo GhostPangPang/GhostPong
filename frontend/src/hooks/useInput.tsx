34e6d302b7894a7e5143edb83b7e32e8685e0f73
Seungsoo Lee <74805318+aLVINlEE9@users.noreply.github.com>
2023-05-16 15:05:17 +0900

    ✨ [Feature] Add useInput hook (#315)

---
+++ frontend/src/hooks/useInput.tsx
@@ -0,0 +1,9 @@
+import { useState, useCallback } from 'react';
+
+export const useInput = (initialValue: string) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  }, []);
+  return { value, onChange };
+};
