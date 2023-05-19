be7419c2d51ee339d20bcc374b63754e1a90c268
aLVINlEE9 <fldh3746@gmail.com>
2023-05-19 19:46:25 +0900

    🔨 refactor: Add validate function option to useInput

--- frontend/src/hooks/useInput.tsx
+++ frontend/src/hooks/useInput.tsx
@@ -1,9 +1,26 @@
 import { useState, useCallback } from 'react';
 
-export const useInput = (initialValue: string) => {
+interface UseInputProps {
+  initialValue: string | undefined;
+  validationFunc?: (value: string) => { isValid: boolean; errorMessage: string };
+}
+
+export const useInput = ({ initialValue, validationFunc }: UseInputProps) => {
   const [value, setValue] = useState(initialValue);
-  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  }, []);
-  return { value, setValue, onChange };
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const onChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = e.target.value;
+      setValue(newValue);
+
+      if (validationFunc) {
+        const { isValid, errorMessage } = validationFunc(newValue);
+        setErrorMessage(isValid ? '' : errorMessage);
+      }
+    },
+    [validationFunc],
+  );
+
+  return { value, onChange, setValue, errorMessage };
 };
