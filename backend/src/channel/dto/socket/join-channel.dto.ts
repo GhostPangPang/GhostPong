533be60946de36ad8bfbafa319efa740d992ea2f
Kimhannah <skqkdldhf98@naver.com>
2023-05-20 13:42:33 +0900

    :green_heart: chore: Modify JoinChannelDto

--- backend/src/channel/dto/socket/join-channel.dto.ts
+++ backend/src/channel/dto/socket/join-channel.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsString, Max, Min } from 'class-validator';
+import { IsNumber, IsString, Matches, Max, Min } from 'class-validator';
 
 import { JoinChannel } from '@/types/channel/socket';
 
@@ -17,4 +17,18 @@ export class JoinChannelDto implements JoinChannel {
   @Min(1)
   @Max(2147483647)
   userId: number;
+
+  /**
+   * @description 유저 닉네임
+   * @example 'hannkim'
+   */
+  @Matches(/^[가-힣a-zA-Z0-9]{1,8}$/, { message: '유효하지 않은 닉네임 입니다.' })
+  nickname: string;
+
+  /**
+   * @description 유저 이미지 url
+   * @example '/asset/profile-1.jpg'
+   */
+  @IsString()
+  image: string;
 }
--- types/channel/socket/join-channel.interface.ts
+++ types/channel/socket/join-channel.interface.ts
@@ -1,4 +1,6 @@
 export interface JoinChannel {
 	channelId: string;
 	userId: number;
+	nickname: string;
+	image: string;
 }
