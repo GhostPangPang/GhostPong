02cb133aab07a1d35992441e109d0f2fcfcfa5c7
aLVINlEE9 <fldh3746@gmail.com>
2023-05-20 20:51:11 +0900

    🔨 refactor: Refactor GameListPage for changed hook

--- frontend/src/pages/GameListPage/GameListPage.tsx
+++ frontend/src/pages/GameListPage/GameListPage.tsx
@@ -1,28 +1,11 @@
 import { Grid } from '@/common';
 import { GameHeader } from './GameHeader';
 import { GameContent } from './GameContent';
-import { useChannelData } from '@/hooks/useChannelData';
 import { useState } from 'react';
 
 export const GameListPage = () => {
   const [cursor, setCursor] = useState(0);
 
-  const { data, refetch } = useChannelData(cursor);
-
-  const handleNextPage = () => {
-    if (cursor + 1 > (data.total ?? 0) / 9) return;
-    setCursor((prevPage) => prevPage + 1);
-  };
-
-  const handlePrevPage = () => {
-    if (cursor - 1 < 0) return;
-    setCursor((prevPage) => prevPage - 1);
-  };
-
-  const handleRefetch = () => {
-    refetch();
-    setCursor(0);
-  };
   return (
     <Grid
       container="grid"
@@ -33,8 +16,8 @@ export const GameListPage = () => {
       gap={4}
       size={{ height: 'auto', padding: 'md', maxWidth: '100rem' }}
     >
-      <GameHeader fetchNextPage={handleNextPage} fetchPreviousPage={handlePrevPage} handleRefetch={handleRefetch} />
-      <GameContent channels={data.channels} />
+      <GameHeader cursor={cursor} setCursor={setCursor} />
+      <GameContent cursor={cursor} />
     </Grid>
   );
 };
