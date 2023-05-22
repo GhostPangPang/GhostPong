947fe0056dada386f63291b745509f54a1de01e3
aLVINlEE9 <fldh3746@gmail.com>
2023-05-23 03:10:39 +0900

    ✨ feat: Add Error Fallback Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,32 +15,38 @@ import { GameListPage } from './pages/GameListPage';
 import { AuthHandler } from './AuthHandler';
 import { Loading } from './common/Loading/Loading';
 import { SocketHandler } from './SocketHandler';
+import { FallbackComponent, logError } from '@/pages/ErrorFallback';
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
 
 function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<Loading />}>
-        <ErrorBoundary fallback={<h1>Error</h1>}>
-          <SocketHandler />
-          <Routes>
-            <Route element={<MainLayout />}>
-              <Route path="/" element={<LobbyPage />} />
-              <Route path="/message" element={<MessagePage />} />
-              <Route path="/channel/list" element={<GameListPage />} />
-              <Route element={<FooterLayout />}>
-                <Route path="/profile/:userId" element={<ProfilePage />} />
-                <Route path="/profile/edit" element={<EditProfilePage />} />
-              </Route>
-            </Route>
-            <Route element={<GameLayout />}>
-              <Route path="/channel/:gameId" element={<GameReadyPage />} />
-            </Route>
-            <Route path="/pre" element={<PrePage />} />
-            <Route path="/auth?/" element={<AuthHandler />} />
-            <Route path="/auth/register" element={<RegisterPage />} />
-            <Route path="/auth/2fa" element={<h1>2fa</h1>} />
-          </Routes>
-        </ErrorBoundary>
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary FallbackComponent={FallbackComponent} onError={logError} onReset={reset}>
+              <SocketHandler />
+              <Routes>
+                <Route element={<MainLayout />}>
+                  <Route path="/" element={<LobbyPage />} />
+                  <Route path="/message" element={<MessagePage />} />
+                  <Route path="/channel/list" element={<GameListPage />} />
+                  <Route element={<FooterLayout />}>
+                    <Route path="/profile/:userId" element={<ProfilePage />} />
+                    <Route path="/profile/edit" element={<EditProfilePage />} />
+                  </Route>
+                </Route>
+                <Route element={<GameLayout />}>
+                  <Route path="/channel/:gameId" element={<GameReadyPage />} />
+                </Route>
+                <Route path="/pre" element={<PrePage />} />
+                <Route path="/auth?/" element={<AuthHandler />} />
+                <Route path="/auth/register" element={<RegisterPage />} />
+                <Route path="/auth/2fa" element={<h1>2fa</h1>} />
+              </Routes>
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
       </Suspense>
     </BrowserRouter>
   );
