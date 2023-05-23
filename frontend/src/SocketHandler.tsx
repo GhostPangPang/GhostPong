adc0433e64fc5bf3688f9db79e1b18458a4a543d
aLVINlEE9 <fldh3746@gmail.com>
2023-05-24 00:38:15 +0900

    ✨ feat: Add join and leave event

--- frontend/src/SocketHandler.tsx
+++ frontend/src/SocketHandler.tsx
@@ -1,14 +1,11 @@
 import { useEffect } from 'react';
 import { connectSocket, disconnectSocket, emitEvent, offEvent, onEvent } from '@/libs/api';
 import { useRecoilCallback, useRecoilValue } from 'recoil';
-import { newMessageIdListState, newMessagesState } from './stores';
+import { newMessageIdListState, newMessagesState, newChannelDataState } from './stores';
 import { socketState } from './stores/socketState';
 import { Message } from '@/dto/message/socket';
-
-const MessageEvent = {
-  LASTVIEW: 'last-message-view',
-  MESSAGE: 'message',
-};
+import { MemberInfo, UserId } from '@/dto/channel/socket';
+import { MessageEvent, ChannelEvent } from './constants';
 
 export const SocketHandler = () => {
   const socket = useRecoilValue(socketState);
@@ -29,6 +26,32 @@ export const SocketHandler = () => {
 
       set(newMessageIdListState, (prev) => [...prev, data.id]);
     }
+  });
+
+  // const updateChatEvent = useRecoilCallback(({ set }) => (data: Chat) => {
+  //   console.log('socket chat', data);
+  //   set(newChannelDataState, (prev) => ({
+  //     ...prev,
+  //     chat: [...prev.chats, data],
+  //   }));
+  // });
+
+  const updateJoinEvent = useRecoilCallback(({ set }) => (data: MemberInfo) => {
+    console.log('socket join', data);
+    set(newChannelDataState, (prev) => ({
+      ...prev,
+      observers: [...prev.observers, data],
+    }));
+  });
+
+  const updateLeaveEvent = useRecoilCallback(({ set }) => (data: UserId) => {
+    console.log('socket leave', data);
+    set(newChannelDataState, (prev) => ({
+      ...prev,
+      observers: prev.observers.filter((observer) => observer.userId !== data.userId),
+      leftPlayer: prev.leftPlayer?.userId === data.userId ? null : prev.leftPlayer,
+      rightPlayer: prev.rightPlayer?.userId === data.userId ? null : prev.rightPlayer,
+    }));
   });
 
   // Init socket
@@ -53,9 +76,37 @@ export const SocketHandler = () => {
     };
   }, [socket]);
 
+  // Turn on channel socket event
+  useEffect(() => {
+    if (!socket.channel) return;
+    // onEvent(ChannelEvent.CHAT, updateChatEvent);
+    onEvent(ChannelEvent.JOIN, updateJoinEvent);
+    onEvent(ChannelEvent.LEAVE, updateLeaveEvent);
+    // onEvent(ChannelEvent.KICK, updateKickEvent);
+    // onEvent(ChannelEvent.BAN, updateBanEvent);
+    // onEvent(ChannelEvent.MUTE, updateMuteEvent);
+    // onEvent(ChannelEvent.PLAYER, updatePlayerEvent);
+    // onEvent(ChannelEvent.ADMIN, updateAdminEvent);
+    // onEvent(ChannelEvent.OWNER, updateOwnerEvent);
+    return () => {
+      // offEvent(ChannelEvent.CHAT);
+      offEvent(ChannelEvent.JOIN);
+      offEvent(ChannelEvent.LEAVE);
+      // offEvent(ChannelEvent.KICK);
+      // offEvent(ChannelEvent.BAN);
+      // offEvent(ChannelEvent.MUTE);
+      // offEvent(ChannelEvent.PLAYER);
+      // offEvent(ChannelEvent.ADMIN);
+      // offEvent(ChannelEvent.OWNER);
+    };
+    // register channel socket on event
+  }, [socket]);
+
   // Turn on game socket event
   useEffect(() => {
     if (!socket.game) return;
+    // game-start(게임 시작 준비하세요 알리기) 이벤트 -> channel state 바꿔주기 [gamePlaying: true]
+
     // register game socket on event
   }, [socket]);
 
