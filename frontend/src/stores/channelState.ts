beadcc48b596d80a935dbafaf244204f67b4da9e
aLVINlEE9 <fldh3746@gmail.com>
2023-05-24 16:30:45 +0900

    ✨ feat: Add channelId to state and selector

--- frontend/src/stores/channelState.ts
+++ frontend/src/stores/channelState.ts
@@ -1,21 +1,23 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 import { Chat, MemberInfo } from '@/dto/channel/socket';
 
 export type ChannelData = {
   name: string;
+  channelId: string;
   leftPlayer: MemberInfo | null;
   rightPlayer: MemberInfo | null;
   observers: MemberInfo[] | [];
   isInGame: boolean;
   currentRole: 'owner' | 'admin' | 'member' | undefined;
   currentUserId: number;
-  chats: Chat[];
+  chats: Chat[] | [];
 };
 
 export const newChannelDataState = atom<ChannelData>({
   key: '/channel',
   default: {
     name: '',
+    channelId: '',
     leftPlayer: null,
     rightPlayer: null,
     observers: [],
@@ -23,5 +25,13 @@ export const newChannelDataState = atom<ChannelData>({
     currentRole: undefined,
     currentUserId: -1,
     chats: [],
+  },
+});
+
+export const newChatSelector = selector<Chat[]>({
+  key: '/chat/selector',
+  get: ({ get }) => {
+    const { chats } = get(newChannelDataState);
+    return [...chats].reverse();
   },
 });
