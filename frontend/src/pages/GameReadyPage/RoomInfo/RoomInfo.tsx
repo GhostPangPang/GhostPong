c831e88673a93744a3b9ea783c14e40684b11438
aLVINlEE9 <fldh3746@gmail.com>
2023-05-26 22:53:24 +0900

    🔨 refactor: Leave channel button available

--- frontend/src/pages/GameReadyPage/RoomInfo/RoomInfo.tsx
+++ frontend/src/pages/GameReadyPage/RoomInfo/RoomInfo.tsx
@@ -1,12 +1,21 @@
 import { Grid, Text, IconButton } from '@/common';
 import { ReactComponent as Lock } from '@/svgs/lock.svg';
 import { ReactComponent as Out } from '@/svgs/out.svg';
+import { useRecoilValue } from 'recoil';
+import { channelDataState, channelIdState } from '@/stores';
+import { useLeaveChannel } from '@/hooks/channel';
+import { useNavigate } from 'react-router-dom';
 
-interface RoomInfoProps {
-  name: string;
-}
+export const RoomInfo = () => {
+  const channelData = useRecoilValue(channelDataState);
+  const channelId = useRecoilValue(channelIdState);
+  const navigate = useNavigate();
+  const { leaveChannel } = useLeaveChannel();
+  const handleOut = () => {
+    leaveChannel(channelId);
+    navigate('/channel/list');
+  };
 
-export const RoomInfo = ({ name }: RoomInfoProps) => {
   return (
     <Grid
       container="flex"
@@ -15,12 +24,12 @@ export const RoomInfo = ({ name }: RoomInfoProps) => {
       justifyContent="space-between"
       size={{ height: '100%', padding: 'md' }}
     >
-      <IconButton>
+      <IconButton onClick={handleOut}>
         <Out />
       </IconButton>
       <Grid container="flex" direction="row" alignItems="center" justifyContent="end">
         <Lock />
-        <Text size="lg">{name}</Text>
+        <Text size="lg">{channelData.name}</Text>
       </Grid>
     </Grid>
   );
