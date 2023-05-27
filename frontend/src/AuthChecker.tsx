94c991d4ec1af44c3e5a04b3bc79c65f35163af2
Chloekkk <skdusdl8804@gmail.com>
2023-05-27 15:17:59 +0900

    :sparkles: feat: Update AuthChecker

--- frontend/src/AuthChecker.tsx
+++ frontend/src/AuthChecker.tsx
@@ -1,18 +1,22 @@
 import { Outlet, useNavigate } from 'react-router-dom';
 import { getAccessToken } from './libs/api/auth';
-import { useLayoutEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { SocketHandler } from './SocketHandler';
 
 export const AuthChecker = () => {
   const navigate = useNavigate();
   const token = getAccessToken();
+  const [isAuth, setIsAuth] = useState(false);
 
-  useLayoutEffect(() => {
-    if (!token) {
-      navigate('/pre');
-    }
-  }, [token]);
+  useEffect(() => {
+    if (token) setIsAuth(true);
+  }, []);
 
+  useEffect(() => {
+    if (isAuth === false) navigate('/pre');
+  }, [isAuth]);
+
+  if (isAuth === false) return <></>;
   return (
     <>
       <SocketHandler />
