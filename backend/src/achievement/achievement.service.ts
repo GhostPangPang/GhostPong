4678fdc2fe946e0c35881ad63ffd227d26941260
anso33 <hyeon131812@gmail.com>
2023-05-27 20:28:26 +0900

    :bug: bug: Fix test code to real code

--- backend/src/achievement/achievement.service.ts
+++ backend/src/achievement/achievement.service.ts
@@ -5,13 +5,13 @@ import { Achievement } from '../entity/achievement.entity';
 import { UserRecord } from '../entity/user-record.entity';
 
 /**
- * 1 : 첫 승리					// 첫 승리 축하드립니다!!
- * 2 : 승리 100회				// 승리 100회 달성!!
- * 3 : 총 게임 횟수 42회		// 게임을 42!!
- * 4 : 패배 횟수 42회			// 패배도 42!!
- * 5 : 첫 친구 만들어 봤을 때	// 드디어 친구가 생겼어요!!
- * 6 : 친구 10명 				// 인싸로 가는 첫 걸음!!
- * 7 : 친구 42명 				// 당신은 마당발!!
+ * 1 : 첫 승리                // 첫 승리 축하드립니다!!
+ * 2 : 승리 100회             // 승리 100회 달성!!
+ * 3 : 총 게임 횟수 42회      // 게임을 42!!
+ * 4 : 패배 횟수 42회         // 패배도 42!!
+ * 5 : 첫 친구 만들어 봤을 때 // 드디어 친구가 생겼어요!!
+ * 6 : 친구 10명              // 인싸로 가는 첫 걸음!!
+ * 7 : 친구 42명              // 당신은 마당발!!
  */
 
 @Injectable()
@@ -52,10 +52,8 @@ export class AchievementService {
     if (result === null) {
       return;
     }
-    // await this.getWinAchievement(userId, result.winCount, manager);
-    await this.getWinAchievement(userId, 1, manager);
-    // await this.getTotalGameAchievement(userId, result.winCount + result.loseCount, manager);
-    await this.getTotalGameAchievement(userId, 42, manager);
+    await this.getWinAchievement(userId, result.winCount, manager);
+    await this.getTotalGameAchievement(userId, result.winCount + result.loseCount, manager);
   }
 
   async checkLoserAchievement(userId: number, manager: EntityManager): Promise<void> {
@@ -63,9 +61,7 @@ export class AchievementService {
     if (result === null) {
       return;
     }
-    // await this.getLoseAchievement(userId, result.loseCount, manager);
-    await this.getLoseAchievement(userId, 42, manager);
-    // await this.getTotalGameAchievement(userId, result.winCount + result.loseCount, manager);
-    await this.getTotalGameAchievement(userId, 42, manager);
+    await this.getLoseAchievement(userId, result.loseCount, manager);
+    await this.getTotalGameAchievement(userId, result.winCount + result.loseCount, manager);
   }
 }
