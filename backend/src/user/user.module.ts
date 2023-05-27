4fddb1aca44954cb54dfd4348cc3d4bcbf3012e1
Kimhannah <skqkdldhf98@naver.com>
2023-05-27 18:12:10 +0900

    :hammer: refactor: Modify guest guard

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module, forwardRef } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from '../auth/auth.module';
 import { AppConfigModule } from '../config/app/configuration.module';
+import { JwtConfigModule } from '../config/auth/jwt/configuration.module';
 import { Achievement } from '../entity/achievement.entity';
 import { GameHistory } from '../entity/game-history.entity';
 import { UserRecord } from '../entity/user-record.entity';
@@ -16,6 +18,8 @@ import { UserService } from './user.service';
     TypeOrmModule.forFeature([User, Achievement, UserRecord, GameHistory]),
     forwardRef(() => AuthModule),
     AppConfigModule,
+    JwtModule.register({}),
+    JwtConfigModule,
   ],
   controllers: [UserController],
   providers: [UserService],
