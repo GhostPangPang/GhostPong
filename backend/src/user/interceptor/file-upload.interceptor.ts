8ca9a1998eb918009a7bfe85629adf99cb683e4f
Jisu <hellojs242@gmail.com>
2023-05-28 18:48:35 +0900

    :hammer: refactor: Modify image upload and serve path #491

--- backend/src/user/interceptor/file-upload.interceptor.ts
+++ backend/src/user/interceptor/file-upload.interceptor.ts
@@ -26,7 +26,7 @@ export class FileUploadInterceptor implements NestInterceptor<void, void> {
         cb(null, true);
       },
       storage: diskStorage({
-        destination: 'public/asset',
+        destination: 'public/image',
         filename: (req, file, cb) => {
           const myId: number = ctx.getRequest().user.userId;
           const extArray = file.mimetype.split('/');
