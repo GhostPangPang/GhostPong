c56740a4cfaf72585d03f1db3f3e0471f43e881c
Jisu <hellojs242@gmail.com>
2023-08-02 21:56:19 +0900

    :hammer: refactor: Integrate env file to backend

--- backend/seeding/data-source.ts
+++ backend/seeding/data-source.ts
@@ -27,11 +27,11 @@ config({
 
   const options: DataSourceOptions = {
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    database: process.env.POSTGRES_NAME,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
     synchronize: true,
     entities: [Auth, User, Friendship, Message, MessageView, UserRecord, GameHistory, BlockedUser, Achievement],
     namingStrategy: new SnakeNamingStrategy(),
--- backend/seeding/reset-db.sh
+++ backend/seeding/reset-db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-DB=$(grep "DB_NAME" .env.local | head -1 | cut -d "=" -f 2)
-HOST=$(grep "DB_HOST" .env.local | cut -d "=" -f 2)
-PORT=$(grep "DB_PORT" .env.local | cut -d "=" -f 2)
-USER=$(grep "DB_USER" .env.local | cut -d "=" -f 2)
-PASSWORD=$(grep "DB_PASSWORD" .env.local | cut -d "=" -f 2)
+DB=$(grep "POSTGRES_NAME" .env.local | head -1 | cut -d "=" -f 2)
+HOST=$(grep "POSTGRES_HOST" .env.local | cut -d "=" -f 2)
+PORT=$(grep "POSTGRES_PORT" .env.local | cut -d "=" -f 2)
+USER=$(grep "POSTGRES_USER" .env.local | cut -d "=" -f 2)
+PASSWORD=$(grep "POSTGRES_PASSWORD" .env.local | cut -d "=" -f 2)
 PGPASSWORD=$PASSWORD psql -p "$PORT" -h "$HOST" -U "$USER" -d postgres -c "drop database $DB" -c "create database $DB"
 
 rm -rf seeding/results;
