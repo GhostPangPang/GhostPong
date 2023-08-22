16633416a91a3b9bf4336fa6cbadc31ffe71bef9
Nayeon Kim <skdusdl8804@gmail.com>
2023-08-22 16:52:31 +0900

    Revert "✨ [Feature] footer 컴포넌트 (#586)"
    This reverts commit 799876f3493fc135002bc135a283f35367c864c4.

--- frontend/src/layout/Footer/Footer.tsx
+++ frontend/src/layout/Footer/Footer.tsx
@@ -1,25 +1,19 @@
-import { ReactComponent as Ghost } from '@/svgs/ghost-sm.svg';
+import { ReactComponent as GithubIcon } from '@/svgs/Github.svg';
+import { ReactComponent as GhostIcon } from '@/svgs/GhostIcon.svg';
 import { Grid, Text, Box } from '@/common';
 
 export const Footer = () => {
   return (
-    <Grid
-      container="flex"
-      direction="column"
-      alignItems="center"
-      rowGap={1.5}
-      size={{ width: '100vw', paddingTB: 4 }}
-      style={{ marginTop: '5rem', backgroundColor: '#1E1E1E' }}
-    >
+    <Grid container="flex" direction="column" rowGap={1.5} size={{ maxWidth: '100rem' }}>
       <Box as="section" height="100%" width="100rem" backgroundColor="transparent">
-        <Grid container="flex" direction="column" justifyContent="center" alignItems="center" gap={1} margin={2}>
-          <Ghost />
+        <Grid container="flex" direction="column" justifyContent="center" alignItems="center" columnGap={3} margin={2}>
           <Text size="xs" color="gray100">
-            © 2023. GhostPong. All rights reserved.
+            © 2023. All rights reserved.
           </Text>
-          <Text size="xxs" color="gray100t">
-            nkim, jiskim, hannkim, san, seungsle
-          </Text>
+          <Grid container="flex" direction="row" justifyContent="center" alignItems="center" columnGap={3} margin={2}>
+            <GithubIcon />
+            <GhostIcon />
+          </Grid>
         </Grid>
       </Box>
     </Grid>
