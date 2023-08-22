370e02276dd5586e5182b8357706270e585c18f9
Nayeon Kim <skdusdl8804@gmail.com>
2023-08-22 22:03:37 +0900

    Revert ":bug: [HotFix] Fix docker-cd.yml"

--- frontend/src/pages/MessagePage/BlockModal/BlockModal.tsx
+++ frontend/src/pages/MessagePage/BlockModal/BlockModal.tsx
@@ -43,7 +43,7 @@ export const BlockModal = ({ isOpen, onClose }: Omit<ModalProps, 'children'>) =>
           <GameInput
             value={nickname}
             onChange={handleNicknameChange}
-            sizes="lg"
+            sizes="sm"
             color="secondary"
             placeholder="플레이어 닉네임"
             style={{ flexGrow: 1 }}
--- frontend/src/pages/MessagePage/FriendsModal/FriendsModal.tsx
+++ frontend/src/pages/MessagePage/FriendsModal/FriendsModal.tsx
@@ -52,7 +52,7 @@ export const FriendsModal = ({ isOpen, onClose }: Omit<ModalProps, 'children'>) 
           <GameInput
             value={nickname}
             onChange={handleNicknameChange}
-            sizes="lg"
+            sizes="sm"
             color="secondary"
             placeholder="플레이어 닉네임"
             style={{ flexGrow: 1 }}
